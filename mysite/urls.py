f98f0125eede09d04e63cc39336a4522bda71fe7
0xFN89 <eighteen089@gmail.com>
2016-01-09 23:26:30 +0530

    passed extra arguments to the view function

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 from mysite.views import current_time
-
+from mysite.views import hours_more
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^time/$',current_time),
+     url(r'^time/$',current_time),
+    url(r'^ahead_time/$',hours_more,{'offset':7}),
+    #we can also use the below pattern
+    #url(r'^ahead_time/([0-9])/$',hours_more),
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render_to_response
 import datetime
+def hours_more(request,offset):
+    offset=int(offset)
+    dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
+    return render_to_response('hours_ahead.html',{'hours_offset':offset,'future_time':dt })
 def current_time(request):
     now = datetime.datetime.now()
     return render_to_response('current_datetime.html',{'current_date':now })
