27706bf475b75c96939decce6461932bc862f7fa
0xFN89 <eighteen089@gmail.com>
2016-01-12 15:09:34 +0530

    raise ValidationError if message contains less than 4 words

--- books/forms.py
+++ books/forms.py
@@ -8,3 +8,9 @@ class ContactForm(forms.Form):
     topic = forms.ChoiceField(choices=TOPIC_CHOICES)
     message = forms.CharField(widget=forms.Textarea())
     sender = forms.EmailField(required=False)
+    def clean_message(self):
+        message = self.cleaned_data.get('message','')
+        num_words = len(message.split())
+        if num_words >4:
+            raise forms.ValidationError("not enough words")
+        return message
